8e47a9e3c98a3b5c7f062ff3e734f64dff99354d
Rien Croonenborghs <rien@croonenborghs.net>
2021-12-27 09:19:02 +1300

    queue sizes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :queue_sizes
+
+  def queue_sizes
+    @queue_sizes = current_user ? current_user.downloads.group(:status).count : {}
+  end
 end
